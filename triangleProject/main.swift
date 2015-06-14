4a7c39c25ed22183cbe2303462207ff26f1d2e8b
aem <amarkon@infinio.com>
2015-06-14 00:25:59 -0400

    Initial Commit

---
+++ triangleProject/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  triangleProject
+//
+//  Created by Adam Markon on 6/14/15.
+//  Copyright © 2015 Adam Markon. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
